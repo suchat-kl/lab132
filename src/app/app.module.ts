c503523ba0631f7b9f606826f1d9e9b82ec039e2
suchat-kl <suchat.kl@kkumail.com>
2021-05-25 19:58:30 +0700

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponentComponent } from './todo-list-component/todo-list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-list-component/todo-list-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-component',
+  templateUrl: './todo-list-component.component.html',
+  styleUrls: ['./todo-list-component.component.css']
+})
+export class TodoListComponentComponent implements OnInit {
+// drinks =new Array<String>();
+drinks:String[]=[];
+dr:String="";
+onclick(){
+  this.drinks.push(this.dr);
+}
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
